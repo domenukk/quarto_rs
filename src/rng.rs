f75e27f508186a705d43216e8b7d6265baa93dd4
Dominik Maier <dmnk@google.com>
2022-07-25 03:23:07 +0200

    more rng copypasta

--- src/rng.rs
+++ src/rng.rs
@@ -30,6 +30,57 @@ impl RomuDuoJrRand {
         self.y_state = self.y_state.wrapping_sub(xp).rotate_left(27);
         xp
     }
+
+    /// Gets a value below the given 64 bit val (inclusive)
+    pub fn below(&mut self, upper_bound_excl: u64) -> u64 {
+        if upper_bound_excl <= 1 {
+            return 0;
+        }
+
+        /*
+        Modulo is biased - we don't want our fuzzing to be biased so let's do it
+        right. See
+        https://stackoverflow.com/questions/10984974/why-do-people-say-there-is-modulo-bias-when-using-a-random-number-generator
+        */
+        let mut unbiased_rnd: u64;
+        loop {
+            unbiased_rnd = self.next();
+            if unbiased_rnd < (u64::MAX - (u64::MAX % upper_bound_excl)) {
+                break;
+            }
+        }
+
+        unbiased_rnd % upper_bound_excl
+    }
+
+    /// Gets a value between the given lower bound (inclusive) and upper bound (inclusive)
+    pub fn between(&mut self, lower_bound_incl: u64, upper_bound_incl: u64) -> u64 {
+        debug_assert!(lower_bound_incl <= upper_bound_incl);
+        lower_bound_incl + self.below(upper_bound_incl - lower_bound_incl + 1)
+    }
+
+    /// Choose an item at random from the given iterator, sampling uniformly.
+    ///
+    /// Note: the runtime cost is bound by the iterator's [`nth`][`Iterator::nth`] implementation
+    ///  * For `Vec`, slice, array, this is O(1)
+    ///  * For `HashMap`, `HashSet`, this is O(n)
+    pub fn choose<I, E, T>(&mut self, from: I) -> T
+    where
+        I: IntoIterator<Item = T, IntoIter = E>,
+        E: ExactSizeIterator + Iterator<Item = T>,
+    {
+        // create iterator
+        let mut iter = from.into_iter();
+
+        // make sure there is something to choose from
+        debug_assert!(iter.len() > 0, "choosing from an empty iterator");
+
+        // pick a random, valid index
+        let index = self.below(iter.len() as u64) as usize;
+
+        // return the item chosen
+        iter.nth(index).unwrap()
+    }
 }
 
 #[cfg(test)]
